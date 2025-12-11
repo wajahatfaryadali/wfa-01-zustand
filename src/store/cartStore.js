c35e77e1345e86a1a8198f402d4a75eea7c611f3
Wajahat Ali <wajahatfaryadali@gmail.com>
2025-12-11 14:07:08 +0500

    persisting the store

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -1,12 +1,22 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 import { createSelectors } from "./createSelector";
 
-const useCartStore = create((set) => ({
-    cart: [],
-    addToCart: (item) => set((state) => ({ cart: [...state.cart, item] })),
-    removeFromCart: (item) => set((state) => ({ cart: state.cart.filter(product => product.id !== item.id) })),
-    clearCart: () => set(() => ({ cart: [] })),
-}))
+const useCartStore = create(
+    persist(
+        (set) => ({
+            cart: [],
+            addToCart: (item) => set((state) => ({ cart: [...state.cart, item] })),
+            removeFromCart: (item) => set((state) => ({ cart: state.cart.filter(product => product.id !== item.id) })),
+            clearCart: () => set(() => ({ cart: [] })),
+        }),
+        {
+            name: 'cart-storage',
+            storage: createJSONStorage(() => localStorage),
+            partialize: (state) => ({ cart: state.cart })
+        }
+    )
+)
 
 export const useCartStoreSelectors = createSelectors(useCartStore)
 
